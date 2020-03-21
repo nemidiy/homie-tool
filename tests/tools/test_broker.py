7fd30c527415616aa16e250bd549ac29ed92dce8
Nicolas Emiliani <n.r.emiliani@gmail.com>
2020-03-21 20:38:27 -0300

    Add dummy test for bolerplate testing

---
+++ tests/tools/test_broker.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('src')
+
+import pytest
+
+from tools.broker import HomieMQTTBrokerClient
+
+def test_dummy():
+	c = HomieMQTTBrokerClient(None, **{'broker_host':'127.0.0.1', 'broker_port': 1883})
