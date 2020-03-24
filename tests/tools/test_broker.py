8c0711a24859d98d884502487687a17698e5d7fd
Nicolas Emiliani <n.r.emiliani@gmail.com>
2020-03-24 19:02:08 -0300

    Black reformatting

--- tests/tools/test_broker.py
+++ tests/tools/test_broker.py
@@ -1,9 +1,11 @@
 import sys
-sys.path.append('src')
+
+sys.path.append("src")
 
 import pytest
 
 from tools.broker import HomieMQTTBrokerClient
 
+
 def test_dummy():
-	c = HomieMQTTBrokerClient(None, **{'broker_host':'127.0.0.1', 'broker_port': 1883})
+    c = HomieMQTTBrokerClient(None, **{"broker_host": "127.0.0.1", "broker_port": 1883})
